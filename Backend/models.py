56bdc0295f6ec3707803a81adc6abeee796f7d51
OxAbla <159329097+OxAbla@users.noreply.github.com>
2024-03-26 11:19:55 +0100

    models.py

--- Backend/models.py
+++ Backend/models.py
@@ -3,20 +3,49 @@ from  __main__ import db
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
     username = db.Column(db.String(20), unique=True)
-    image_file = db.Column(db.String(20), default='default.jpg', nullable=False)
+    name = db.Column(db.String(20),nullable=False)
+    familly_name = db.Column(db.String(20),nullable=False)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(20))
+    gender = db.Column(db.String(10))
+    birthday = db.Column(db.Date,nullable=False)
+    image_file = db.Column(db.Text,nullable=False)
 
-    def __init__(self, username, email, image_file, password):
+    def __init__(self, username,name,familly_name, email, password,gender,birthday,image_file):
+        self.username = username
+        self.email = email
+        self.name = name
+        self.familly_name = familly_name
+        self.image_file = image_file
+        self.password = password
+        self.gender = gender
+        self.birthday = birthday
+
+class Therapist(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    username = db.Column(db.String(20), unique=True)
+    name = db.Column(db.String(20),nullable=False)
+    familly_name = db.Column(db.String(20),nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(20))
+    gender = db.Column(db.String(10))
+    birthday = db.Column(db.Date)
+    image_file = db.Column(db.Text, nullable=True)
+    cv = db.Column(db.LargeBinary, nullable=True)
+    def __init__(self, username, email, password,gender,birthday,image_file,cv):
         self.username = username
         self.email = email
         self.image_file = image_file
         self.password = password
+        self.gender = gender
+        self.birthday = birthday
+        self.cv = cv
 
+class MedicalFile(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    patient_id = db.Column(db.Integer,nullable=False)
+    file_content = db.Column(db.LargeBinary)
+    def __init__(self,patient_id,file_content):
+        self.patient_id = patient_id
+        self.file_content = file_content
 
-class Therapist(db.Model):
-    therapist_id = db.Column(db.Integer,primary_key=True,nullable=False)
-    username = db.Column(db.String(20),unique=True,nullable=False)
-    email = db.Column(db.String(20),unique=True,nullable=False)
-    image_file = db.Column(db.String(120),default= 'default.jpg',nullable=False)
-    password = db.Column(db.String(20))
