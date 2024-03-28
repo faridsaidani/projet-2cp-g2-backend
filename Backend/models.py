d4775c662ae377b47ac8a4bdb96688da4cf28b71
OxAbla <159329097+OxAbla@users.noreply.github.com>
2024-03-28 12:50:13 +0100

    therapist

--- Backend/models.py
+++ Backend/models.py
@@ -34,8 +34,9 @@ class Therapist(db.Model):
     birthday = db.Column(db.Date)
     image_file = db.Column(db.Text, nullable=True)
     cv = db.Column(db.LargeBinary, nullable=True)
+    selected = db.Column(db.Boolean)
 
-    def __init__(self, username, email, name, familly_name, password, gender, birthday, image_file,cv):
+    def __init__(self, username, email, name, familly_name, password, gender, birthday, image_file,cv,selected):
         self.username = username
         self.name = name
         self.familly_name = familly_name
@@ -46,6 +47,7 @@ class Therapist(db.Model):
         self.birthday = birthday
         self.image_file = image_file
         self.cv=cv
+        self.selected=selected
 
 
 class MedicalFile(db.Model):
