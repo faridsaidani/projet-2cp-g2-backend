b9ae9e341a2fb3f4de962d79aa08f04de259cf00
Chaima <C_aouchiche@estin.dz>
2024-04-05 22:37:13 +0200

    update appointement

--- Backend/app/Routes/appointmentRoute.py
+++ Backend/app/Routes/appointmentRoute.py
@@ -1,9 +1,6 @@
 from ..create_app import db
-from flask import Blueprint
+from flask import Blueprint,request,jsonify,session
 from ..models  import Appointment
-from validate_email import validate_email
-import base64
-
 
 appointmentRoute = Blueprint('appointmentRoute', __name__,url_prefix='/appointment')
 
@@ -12,6 +9,36 @@ appointmentRoute = Blueprint('appointmentRoute', __name__,url_prefix='/appointme
 
 #update
 # write it here
+@appointmentRoute.route('/update/<int:id>', methods=['PUT'])
+def update_appointment(id):
+    appointment = Appointment.query.get(id)
+    if appointment:
+        data = request.form
+        # Check if the provided name conflicts with existing appointments
+        
+        if "name" in data and data["name"] != appointment.name:
+            existing_appointment = Appointment.query.filter_by(name=data["name"]).first()
+            if existing_appointment:
+                return jsonify({'error': 'An appointment with this name already exists'}), 400
+            else:
+                appointment.name = data["name"]
 
+        # Check if the provided date conflicts with existing appointments
+        if "date" in data:
+            new_date = data["date"]
+            existing_appointment = Appointment.query.filter_by(date=new_date).first()
+            if existing_appointment:
+                return jsonify({'error': 'An appointment already exists at this date'}), 400
+            else:
+                appointment.date = new_date
+
+        # Update therapist ID and patient ID
+        appointment.therapist_id = data["therapist_id"]
+        appointment.patient_id = data["patient_id"]
+
+        db.session.commit()
+        return jsonify({'message': 'Appointment updated successfully'})
+    else:
+        return jsonify({'error': 'Appointment not found'}), 404
 #delete
 # write it here
