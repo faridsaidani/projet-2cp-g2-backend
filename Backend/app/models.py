557be57ec9ac30f2d741f4858f42928de6d6fded
OxAbla <159329097+OxAbla@users.noreply.github.com>
2024-04-18 23:35:19 +0200

    modelsRoute

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -1,4 +1,5 @@
 from .create_app  import db
+from datetime import datetime 
 
 
 class Patient(db.Model):
@@ -77,3 +78,103 @@ class Appointment(db.Model):
        self.date = date
        self.therapist_id = therapist_id
        self.patient_id = patient_id
+
+# models that we need it for the chat 
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
+    therapist_id = db.Column(db.Integer, db.ForeignKey('therapist.id'), nullable=False)
+    messages = db.relationship('Message', backref='conversation')
+    def __init__(self, patient_id, therapist_id):
+        self.patient_id = patient_id
+        self.therapist_id = therapist_id
+
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_type = db.Column(db.String(20), nullable=False)  # 'therapist' or 'patient'
+    sender_id = db.Column(db.Integer, nullable=False)
+    recipient_type = db.Column(db.String(20), nullable=False)
+    recipient_id = db.Column(db.Integer, nullable=False)
+    Conversation_id = db.Column(db.Integer, db.ForeignKey('conversation.id'), default = None) 
+    content = db.Column(db.Text, nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    def __init__(self, sender_type, sender_id,recipient_type,content,time):
+        self.sender_type = sender_type
+        self.sender_id = sender_id
+        self.recipient_type = recipient_type
+        self.content = content
+        self.time = time
+
+# the callender
+# class Callender(db.Model):
+#     id = db.Column(db.Integer,primary_key=True)
+#     Therapist_id = db.Column(db.Integer)
+#     def __init__(self,therapist_id):
+#         self.therapist_id = therapist_id
+
+class Event(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    therapist_id = db.Column(db.Integer)
+    patient_id = db.Column(db.Integer)
+    title = db.Column(db.String(100), nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+    def __init__(self,therapist_id,patient_id,title,start_date,end_date):
+        self.therapist_id = therapist_id
+        self.patient_id = patient_id
+        self.title = title
+        self.start_date = start_date
+        self.end_date = end_date
+
+# notifications
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # therapist table and patient table are not the same
+    recipient_type = db.Column(db.String(20),nullable=False)
+    recipient_id = db.Column(db.Integer)
+    message = db.Column(db.String(255))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+    read = db.Column(db.Boolean, default=False)
+
+    def __init__(self, recipient_id, message,timestamp,read,recipient_type):
+        self.recipient_id = recipient_id
+        self.recipient_type = recipient_type
+        self.message = message
+        self.timestamp = timestamp
+        self.read = read
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    def __init__(self, name, content,date):
+        self.name = name
+        self.content = content
+        self.date = date
+
+# id username email password 
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(120),unique=True,nullable=False)
+    password = db.Column(db.Text,nullable=False)
+    image_file = db.Column(db.Text, nullable=False)
+    name = db.Column(db.String(20),nullable=False)
+    familly_name = db.Column(db.String(20),nullable=False)
+    gender = db.Column(db.String(20),nullable=False)
+
+    def __init__(self, username,email,password,image_file,name,familly_name,gender):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.image_file = image_file
+        self.name = name
+        self.familly_name = familly_name
+        self.gender = gender
+
+
+
