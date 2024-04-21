f355002468f85a222956bf6e815f4ff47ec72265
OUALIzouina <117228257+OUALIzouina@users.noreply.github.com>
2024-04-21 14:54:01 +0100

    contactus

---
+++ Front-end/src/components/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactUsBody from "../components/CONTACTUS/ContactUsBody";
+
+const ContactUs = () => {
+  return (
+    <div>
+       <ContactUsBody />
+    </div>
+  )
+}
+
+export default ContactUs
