835035fe9746a665949c3fe2e628b428dbfba70b
Chaima <C_aouchiche@estin.dz>
2024-04-23 14:06:36 +0200

    inished video calls

--- Backend/app/create_app.py
+++ Backend/app/create_app.py
@@ -1,15 +1,13 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from configs import Config
+from config import Config
 from flask_socketio import SocketIO
 from flask_mail import Mail
-import asyncio
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
-socketio = SocketIO()
-mail = Mail()
+socketio = SocketIO()  # SocketIO instance
 
 def create_app():
     app = Flask(__name__)
@@ -17,19 +15,19 @@ def create_app():
 
     db.init_app(app)
     bcrypt.init_app(app)
-    # socketio.init_app(app)
-    mail.init_app(app)
+    socketio.init_app(app)  # Initialize SocketIO with the Flask app
+   # Mail.init_app(app)
 
     from .Routes.appointmentRoute import appointmentRoute
     from .Routes.patientRoute import patientRoute
     from .Routes.blogsRoute import blogsRoute
-    from .Routes.callenderRoute import callenderRoute
+    #from .Routes.callenderRoute import callenderRoute
     from .Routes.therapistRoute import therapistRoute
-    from .Routes.messageRoute import messageRoute
-    from .Routes.notificationRoute import notificationRoute
+    #from .Routes.messageRoute import messageRoute
+    #from .Routes.notificationRoute import notificationRoute
     from .Routes.adminRoute import adminRoute
-    from .Routes.contactRoute import contactRoute
-    
+    #from .Routes.contactRoute import contactRoute
+    from .Routes.video_calls import video_call_route
 
     with app.app_context():
         db.create_all()
@@ -37,12 +35,14 @@ def create_app():
     app.register_blueprint(patientRoute)
     app.register_blueprint(therapistRoute)
     app.register_blueprint(appointmentRoute)
-    app.register_blueprint(notificationRoute)
+   # app.register_blueprint(notificationRoute)
     app.register_blueprint(blogsRoute)
-    app.register_blueprint(callenderRoute)
-    app.register_blueprint(messageRoute)
+    #app.register_blueprint(callenderRoute)
+    #app.register_blueprint(messageRoute)
     app.register_blueprint(adminRoute)
-    app.register_blueprint(contactRoute)
+    #app.register_blueprint(contactRoute)
+    app.register_blueprint(video_call_route)  # Register video_call_route
+    # ^ Don't forget to register your video_call_route blueprint here
 
     @app.route('/', methods=['GET'])
     def home():
